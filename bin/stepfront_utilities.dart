06e0ce45c7174733c005b1b9f4eee29ea35af8f7
21kkfy <21kkfy@gmail.com>
2023-04-07 13:45:17 +0300

    Init commit

---
+++ bin/stepfront_utilities.dart
@@ -0,0 +1,5 @@
+import 'package:stepfront_utilities/stepfront_utilities.dart' as stepfront_utilities;
+
+void main(List<String> arguments) {
+  print('Hello world: ${stepfront_utilities.calculate()}!');
+}

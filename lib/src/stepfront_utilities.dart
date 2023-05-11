ecc073076f4d195937d1f8055628ac7868f0c34a
21kkfy <21kkfy@gmail.com>
2023-05-11 10:37:14 +0300

    Added encrypt

--- lib/src/stepfront_utilities.dart
+++ lib/src/stepfront_utilities.dart
@@ -1,10 +1,11 @@
+import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui';
 
+import 'package:encrypt/encrypt.dart';
 import 'package:get/get.dart';
-import 'package:stepfront_utilities/src/app_colors.dart';
-import 'dart:io';
 
+import 'package:stepfront_utilities/src/app_colors.dart';
 import 'package:stepfront_utilities/src/stepfront_exception_model.dart';
 
 class Utilities {
@@ -78,5 +79,50 @@ class Utilities {
 
     Get.updateLocale(Locale(languageCode, countryCode));
     return;
+  }
+
+  /// Checks if the given [time] has already passed.
+  ///
+  /// This method compares the given [time] to the current system time. If the
+  /// [time] has already passed, the method returns `false`. Otherwise, it returns
+  /// `true`.
+  ///
+  /// The [time] argument is a [DateTime] object that represents the time to be
+  /// checked.
+  ///
+  /// Returns a [bool], where `true` indicates that the [time] has not yet passed,
+  /// and `false` indicates that the [time] has already passed.
+  bool isItTimeYet(DateTime time) {
+    if (time.millisecondsSinceEpoch >= DateTime.now().millisecondsSinceEpoch) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  String encrypt(String plainText) {
+    /// key length as 32
+    final key = Key.fromUtf8("0rL7'Q')Y#K9t0z0kWYmWAtOQ#=aN{hK");
+    final iv = IV.fromLength(16);
+
+    final encrypter = Encrypter(AES(key));
+
+    final encrypted = encrypter.encrypt(plainText, iv: iv);
+
+    print(encrypted.base64);
+    return encrypted.base64;
+  }
+
+  String decrypt(String encryptedString) {
+    Encrypted encrypted = Encrypted.fromBase64(encryptedString);
+
+    /// key length as 32
+    final key = Key.fromUtf8("0rL7'Q')Y#K9t0z0kWYmWAtOQ#=aN{hK");
+    final iv = IV.fromLength(16);
+
+    final encrypter = Encrypter(AES(key));
+
+    final decrypted = encrypter.decrypt(encrypted, iv: iv);
+    return decrypted;
   }
 }

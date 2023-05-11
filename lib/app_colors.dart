f8124b52bb5a09b88f00c2cee3ff8c3835000275
21kkfy <21kkfy@gmail.com>
2023-05-11 11:10:35 +0300

    Changes

---
+++ lib/app_colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  /// This color's hexCode: 0xffFF6565
+  static Color get warningRed => const Color(0xffFF6565);
+
+  /// This color's hexCode: 0xff1CB652
+  static Color get active => const Color(0xff1CB652);
+
+  /// This color's hexCode: 0xffFFC064
+  static Color get yellow => const Color(0xffFFC064);
+
+  /// This color's hexCode: 0xffFFFFFF
+  static Color get white => const Color(0xffFFFFFF);
+}
